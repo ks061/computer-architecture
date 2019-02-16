fd4d67055819e4c7ed1ce04e0046b20d71b513bf
Kartikeya Sharma <ks061@bucknell.edu>
2019-02-15 19:55:57 -0500

    Lab 4, Exercise 1: Added header and method comments.

--- Labs/Lab04/calc.c
+++ Labs/Lab04/calc.c
@@ -1,11 +1,28 @@
+/* Kartikeya Sharma
+ * February 5, 2019 @ 1 PM
+ * lab 04 - calc.c
+ * compile with: make calc
+ * notes: This program represents
+ *        a calculator that can add, 
+ *        multiply, or divide numbers
+ *        (float or int) and can find
+ *        the length of strings.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define OP_INDEX 1
-#define ARGS_START_INDEX 2
-#define MIN_CMD_LINE_ARGS 3
+#define OP_INDEX 1 // Index of calculator operation in the array of command line arguments
+#define ARGS_START_INDEX 2 // Starting index of operands in the array of command line arguments
+#define MIN_CMD_LINE_ARGS 3 // Minimum number of command line arguments 
 
+/*
+ * Calculates the length of strings passed in as command line arguments
+ *
+ * argc: number of arguments passed into the command line
+ * argv: array of arguments passed into the command line
+ */
 void len_calc(int argc, char* argv[])
 {
     int length = 0;
@@ -16,6 +33,13 @@ void len_calc(int argc, char* argv[])
     printf("%d\n", length);
 }
 
+/*
+ * Calculates the sum of numbers (integers or floats) passed in as
+ * command line arguments
+ *
+ * argc: number of arguments passed into the command line
+ * argv: array of arguments passed into the command line
+ */
 void add_calc(int argc, char* argv[])
 {
     float sum = 0.0;
@@ -26,6 +50,13 @@ void add_calc(int argc, char* argv[])
     printf("%.6f\n", sum);
 }
 
+/*
+ * Calculates the product of numbers (integers or floats) passed in as
+ * command line arguments
+ *
+ * argc: number of arguments passed into the command line
+ * argv: array of arguments passed into the command line
+ */
 void mult_calc(int argc, char* argv[])
 {
     float product = 1;
@@ -36,6 +67,13 @@ void mult_calc(int argc, char* argv[])
     printf("%.6f\n", product);
 }
 
+/*
+ * Calculates the quotient of numbers (integers or floats) passed in as
+ * command line arguments
+ *
+ * argc: number of arguments passed into the command line
+ * argv: array of arguments passed into the command line
+ */
 void div_calc(int argc, char* argv[])
 {
     float quotient = atof(argv[ARGS_START_INDEX]);
@@ -46,13 +84,27 @@ void div_calc(int argc, char* argv[])
     printf("%.6f\n", quotient);
 }
 
+/*
+ * Prints an error message indicating that the user has
+ * provided less than the minimum number of required number
+ * of operands needed for the specified calculator operation
+ *
+ * argc: number of arguments passed into the command line
+ * argv: array of arguments passed into the command line
+ */
 void insuff_num_operands(int argc, char* argv[]) {
     printf("Unsupported arguments for the %s function.\
-                     %d were needed, yet %d were provided.",\
-                     argv[OP_INDEX], MIN_CMD_LINE_ARGS + 1,\
-                     argc);
+            %d were needed, yet %d were provided.",\
+            argv[OP_INDEX], MIN_CMD_LINE_ARGS + 1,\
+            argc);
 }
 
+/*
+ * Runs the main calculator functionality
+ *
+ * argc: number of arguments passed into the command line
+ * argv: array of arguments passed into the command line
+ */
 int main(int argc, char* argv[]) 
 {
     if (argc < MIN_CMD_LINE_ARGS) { 
