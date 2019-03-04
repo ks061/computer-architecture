790bce6b3eac2bf17c8e0118c55d9229fa24a957
Kartikeya Sharma <ks061@bucknell.edu>
2019-03-04 18:04:52 -0500

    Lab 6, Exercise 1

--- Labs/Lab06/xfind.s
+++ Labs/Lab06/xfind.s
@@ -19,7 +19,7 @@
 # THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 #
-# Student name:
+# Student name: Kartikeya Sharma
 #
 #
 # This program uses a procedure xfind to find a particular character.
@@ -34,18 +34,24 @@
 string: .asciiz "Where is my xbox 360?"
 # string: .asciiz "None of that character in here."
 
+x:      .byte 'x'
+
 	.text
 
 main:
-	# Prepare for procedure call
+        # Prepare for procedure call
 	addi	$sp, $sp, -4		# saving registers
 	sw	$ra, 0($sp)
 	
 	la	$a0, string		# load address of string
 	jal	xfind			# call function convert
 
-#   write code here to print the result of the 
-#   call to xfind
+	add $a0, $v0, $zero		# set the argument of the syscall
+                            		# to be the return value of the
+                            		# xfind function                     		
+	addi $v0, $zero, 34		# syscall code to print hexadecimal
+                            		# values (format of memory address)
+	syscall 			# make syscall
 
 	lw	$ra, 0($sp)		# restoring registers
 	addi	$sp, $sp, 4
@@ -54,4 +60,61 @@ main:
 	syscall
 
 # write the code of function xfind after this comment
+# 
+# Algorithm
+# copy starting memory address from $a0 into $t0
+# load x byte into $t1
+# loop:
+# 	load byte from address $t0 into $t2
+#       if $t1 is 0 (read in null terminator):
+# 		load -1 into $v0
+# 		jump to end_loop
+# 	
+# 	if $t1 == $t2:
+# 		load $t0 into $v0
+#               jump to end_loop
+# 	increment the memory address $t0 by 1
+# 	jump to loop
+# end_loop:
+#       jr $ra
+# 
+# Assignment
+# $a0 – The starting memory address of the string.
+#       This address will be placed in the register
+#        by the main program which calls your procedure.
+# $v0 – The result (a valid address or the constant -1).
+# $ra – The return address for the procedure.
+#       The instruction jr $ra will use to go back to
+#       the caller with
+# $t0 - The address from where a byte is being read
+# $t1 - The value of the 'x' byte
+# $t2 - The value of the byte being read in 
 xfind:
+	add $t0, $a0, $zero # load the starting address of the string into $t0
+        lb $t1, x($zero) # load the value of 'x' into $t1
+        
+        loop: # looping through bytes
+                lb $t2, 0($t0) # load the byte being read in
+                
+                
+                bne $t2, $zero, check_byte # if the byte read is not a null 
+                			   # character continue with checking the byte
+                addi $v0, $zero, -1 # if the byte read in is a null character
+                		    # set the return value to -1
+                j end_loop # jump to after the end of the loop
+		
+		
+		check_byte: bne $t1, $t2, increment # if the byte read in is not x, continue
+						    # with incrementing the memory address by 1
+		add $v0, $t0, $zero # if the byte read in is x, set  the return value to the 
+				    # memory address of the byte
+		j end_loop # jump to after the end of the loop
+
+
+		increment: addi $t0, $t0, 1 # increment the memory address of the byte
+					    # being read by 1
+		j loop # loop back to the start of the loop
+	end_loop: 
+	
+		jr $ra # jump to the return address in $ra
+		
