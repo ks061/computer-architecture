f43f3455c9a28f3613efd644a6152b9a5f98e235
Kartikeya Sharma <ks061@bucknell.edu>
2019-04-09 06:22:02 -0400

    Lab 11

---
+++ Labs/Lab11/over5.c
@@ -0,0 +1,37 @@
+/*
+ * CSCI 206 Computer Organization & Programming
+ * Author: L. Felipe Perrone
+ * Date: 2011-08-29
+ * Copyright (c) 2011 Bucknell University
+ *
+ * Permission is hereby granted, free of charge, to any individual or
+ * institution obtaining a copy of this software and associated
+ * documentation files (the "Software"), to use, copy, modify, and
+ * distribute without restriction, provided that this copyright and
+ * permission notice is maintained, intact, in all copies and supporting
+ * documentation.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL BUCKNELL UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES
+ * OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+	char str_source[25] = "I am going to lab today.";
+ 	char str_dest[10];
+        char str_overflow[15];
+        
+	strcpy(str_dest, str_source);
+
+        printf("str_dest: %s\n", str_dest);
+        printf("str_overflow: %s\n", str_overflow);
+
+	return(0);
+}
